52601b0a2522fa4000aecd6f7842a2388070b29f
shreyanssoni <soni21.shreyans@gmail.com>
2022-06-09 22:43:10 +0530

    fixes in design

--- src/form.js
+++ src/form.js
@@ -9,7 +9,7 @@ export default function Thisform() {
   const [email, setEmail] = useState(null);
   const [contact, setContact] = useState(null);
   const [date, setDate] = useState(null);
-  const [val, setVal] = useState(false);
+  const [val, setVal] = useState([false, "red"]);
 
   let handlesubmit = async (e)=>{
     e.preventDefault();
@@ -28,20 +28,24 @@ export default function Thisform() {
       });
       if( response.status === 200){
         setMessage("Successfully Submitted.")
-        setVal(true)
+        setVal([true, "text-success"])
       } else {
-        setMessage("Error Occured")
+        setMessage("An error occured");
+        setVal([false, "text-danger"])
       }
     } catch(err) {
       console.log(err)
+      setMessage("Error Occured");
+      setVal([false, "text-danger"])
     }
   }
 
   return (
     <div className="form">
+      <h3 className="head mb-4 text-center"> Appointment Form </h3>
       <Form action="http://localhost:5000/" method="POST">
         <Form.Group className="mb-3">
-          <Form.Label>Name</Form.Label>
+          <Form.Label className="font-weight-bold">Name:</Form.Label>
           <Form.Control type="text" name="name" 
           placeholder="Enter name"
           onChange={(e) => setName(e.target.value)}
@@ -49,31 +53,31 @@ export default function Thisform() {
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email Address</Form.Label>
+          <Form.Label className="font-weight-bold">Email Address:</Form.Label>
           <Form.Control type="email" name="email" placeholder="Enter email address" 
           onChange={(e) => setEmail(e.target.value)}
           required/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label>Contact Number</Form.Label>
+          <Form.Label className="font-weight-bold">Contact Number:</Form.Label>
           <Form.Control type="text" name="contact" placeholder="Enter your Contact Number"
           onChange={(e) => setContact(e.target.value)}
           />
         </Form.Group>
         
         <Form.Group className="mb-3">
-          <Form.Label>Date</Form.Label>
+          <Form.Label className="font-weight-bold">Date:</Form.Label>
           <Form.Control type="date" name="date"
           onChange={(e) => setDate(e.target.value)}
           />
         </Form.Group>
 
-        <Button variant="primary" disabled={val} onClick={handlesubmit}>
+        <Button variant="primary" disabled={val[0]} onClick={handlesubmit}>
           Submit
         </Button>
       </Form>
-      <p className="message">
+      <p className = {`message ${val[1]}`}>
         {message}
       </p>
       <p className="short">by <a href="https://github.com/shreyanssoni" target="_blank">Shreyans Soni</a> </p>
